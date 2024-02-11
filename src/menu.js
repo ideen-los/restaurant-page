15a305e151ffc539895161b59d5b3e9016c55a4c
Robert <90513724+ideen-los@users.noreply.github.com>
2024-02-11 22:53:17 +0100

    ENH: add logic for module menu.js

--- src/menu.js
+++ src/menu.js
@@ -1,23 +1,33 @@
-function menu() {
-  const headline = document.createElement("h1");
-  headline.textContent = "We are here";
-
-  const map = document.createElement("iframe");
-  map.setAttribute(
-    "src",
-    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8794.437612496251!2d8.232631205603688!3d49.997448157528446!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47bd975c7c834703%3A0xf098ff597aeeb48e!2sthe%20niu%20Mood!5e0!3m2!1sde!2sde!4v1707250724535!5m2!1sde!2sde"
-  );
-  map.style.width = "600px";
-  map.style.height = "450px";
-
-  const para1 = document.createElement("p");
-  para1.innerHTML = "Awesome Restaurant</br>A Street name 65</br>51187 Mainz";
-
-  return {
-    headline,
-    map,
-    para1,
-  };
+export class FoodItem {
+  constructor(id, headline, imgURL, price) {
+    this.id = id;
+    this.headline = headline;
+    this.imgURL = imgURL;
+    this.price = price;
+  }
 }
 
-export default menu;
+export function generateFoodItemHTML(foodItems) {
+  const container = document.createElement("div");
+
+  foodItems.forEach((item) => {
+    const itemElement = document.createElement("div");
+    itemElement.classList.add("food-item", `food-item-id-${item.id}`);
+
+    const itemName = document.createElement("h2");
+    itemName.textContent = item.headline;
+    itemElement.appendChild(itemName);
+
+    const itemImage = new Image();
+    itemImage.src = item.imgURL;
+    itemElement.appendChild(itemImage);
+
+    const itemPrice = document.createElement("p");
+    itemPrice.textContent = item.price;
+    itemElement.appendChild(itemPrice);
+
+    container.appendChild(itemElement);
+  });
+
+  return container;
+}
